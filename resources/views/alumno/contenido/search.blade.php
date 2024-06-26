69efbc6b9a47486b32bf13d89faeaa30b068a8ab
mmontesh <gonzalezjimenezmarcoantonio@gmail.com>
2024-06-26 16:43:09 -0600

    fifth project updtate

--- resources/views/alumno/contenido/search.blade.php
+++ resources/views/alumno/contenido/search.blade.php
@@ -15,20 +15,39 @@
         @if ($results->isNotEmpty())
             <div class="row">
                 @foreach ($results as $material)
-                    <div class="col-md-4 mb-3">
+                    <div class="mb-3" style="max-width: 540px;">
                         <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $material->titulo }}</h5>
-                                <p class="card-text">
-                                    Autor: {{ $material->autor->nombre_autor ?? 'N/A' }}<br>
-                                    Docente: {{ $material->docente->nombre_docente ?? 'N/A' }}<br>
-                                    Academia: {{ $material->academia->nombre_academia ?? 'N/A' }}<br>
-                                    Asignatura: {{ $material->asignatura->nombre_asignatura ?? 'N/A' }}<br>
-                                    Tipo de Contenido: {{ $material->tipoContenido->nombre_tipo_contenido ?? 'N/A' }}<br>
-                                    Fecha de Publicación: {{ $material->fecha_publicacion }}
-                                </p>
-                                <a href="{{ route('alumno.contenido.show', $material->id) }}" class="btn btn-primary">Ver
-                                    Material</a>
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    @if ($material->imagen)
+                                        <img src="{{ asset('storage/' . $material->imagen) }}"
+                                            class="img-fluid rounded-start" alt="{{ $material->nombre }}">
+                                    @else
+                                        Sin imagen
+                                    @endif
+
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-center">{{ $material->titulo }}</h5>
+                                        <p class="card-text">
+                                            Autor: {{ $material->autor->nombre_autor ?? ' ' }}
+                                            {{ $material->autor->apellido_paterno ?? ' ' }}
+                                            {{ $material->autor->apellido_materno ?? ' ' }}<br>
+                                            Docente: {{ $material->docente->nombre_docente ?? ' ' }}
+                                            {{ $material->docente->apellido_paterno ?? ' ' }}
+                                            {{ $material->docente->apellido_materno ?? ' ' }}<br>
+                                            Academia: {{ $material->academia->nombre_academia ?? ' ' }}<br>
+                                            Asignatura: {{ $material->asignatura->nombre_asignatura ?? ' ' }}<br>
+                                            Tipo de Contenido:
+                                            {{ $material->tipoContenido->nombre_tipo_contenido ?? ' ' }}<br>
+                                            Fecha de Publicación: {{ $material->fecha_publicacion }}
+                                        </p>
+                                        <a href="{{ route('alumno.contenido.show', $material->id) }}"
+                                            class="btn btn-primary">Ver
+                                            Material</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
